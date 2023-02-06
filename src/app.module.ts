2ad54acc00285461dc85dff844db1f90325a6b25
PilHliP211 <46680252+PilHliP211@users.noreply.github.com>
2023-02-06 15:46:57 +0000

    necord dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { NecordModule } from 'necord';
 import { Module } from '@nestjs/common';
+import { GatewayIntentBits } from 'discord.js';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DISCORD_TOKEN } from './bot/environment.vars';
 
 @Module({
-  imports: [],
+  imports: [
+    NecordModule.forRoot({
+      token: DISCORD_TOKEN,
+      intents: [GatewayIntentBits.Guilds]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/bot/environment.vars.ts
@@ -0,0 +1,6 @@
+export const USER_ROLE_ID: string = process.env.USER_ROLE_ID;
+export const DEFAULT_COMPUTE_ZONE: string = process.env.DEFAULT_COMPUTE_ZONE;
+export const GUILD_ID: string = process.env.DISCORD_GUILD_ID;
+export const DISCORD_APPLICATION_ID: string = process.env.DISCORD_APPLICATION_ID;
+export const DISCORD_PUBLIC_KEY: string = process.env.DISCORD_PUBLIC_KEY;
+export const DISCORD_TOKEN: string = process.env.DISCORD_TOKEN;
