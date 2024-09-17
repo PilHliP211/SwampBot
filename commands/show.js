e4313f258bca4daed637f8bac0cbf0dcc35d3ff9
PilHliP211 <46680252+PilHliP211@users.noreply.github.com>
2024-09-17 18:05:20 +0000

    feat: add show command set

---
+++ commands/show.js
@@ -0,0 +1,36 @@
+const { SlashCommand, CommandOptionType } = require('slash-create');
+
+const USER_ROLE_ID = process.env.USER_ROLE_ID;
+
+module.exports = class ServersCommand extends SlashCommand {
+  constructor(creator) {
+    super(creator, {
+      name: 'show',
+      description: 'Shows images from various sources',
+      guildID: process.env.DISCORD_GUILD_ID,
+      options: [
+        {
+          name: 'baby',
+          description: 'Pictures of baby',
+          type: CommandOptionType.SUB_COMMAND
+        }
+      ]
+    });
+  }
+
+  async run(ctx) {
+    console.log('hit the run');
+    var subcommand = ctx.subcommands[0];
+    console.log('subcommand:' + subcommand);
+
+    if (subcommand == "baby" && (ctx.member.roles.includes(USER_ROLE_ID))) {
+      console.log('baby...');
+      await ctx.acknowledge(true);
+      ctx.send("pong");
+    }
+    else {
+      console.log("Unrecognized Command or Insufficient Permissions");
+      ctx.send("Unrecognized Command or Insufficient Permissions");
+    }
+  }
+}
